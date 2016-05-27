ec845ce349a6a27e9bbf9e24d285f830adb6a750
renzon <renzon@gmail.com>
2016-05-27 14:44:05 -0300

    #3 Fetch orking for both origin and destin

--- ndb_relations/relations.py
+++ ndb_relations/relations.py
@@ -15,21 +15,32 @@ class Relation(PolyModel):
         model_future = key.get_async()
         if args:
             name, query = args[0]
-            query.filter(cls.destin == key)
-            origin_keys = [relation.origin for relation in query.fetch()]
-            origin_futures = ndb.get_multi_async(origin_keys)
-            model = model_future.get_result()
-            cls._set_relation_property(model, name, origin_futures)
+            if key.kind() == cls.origin._kind:
+                query.filter(cls.origin == key)
+                origin_keys = [relation.destin for relation in query.fetch()]
+                origin_futures = ndb.get_multi_async(origin_keys)
+                model = model_future.get_result()
+                cls._set_origin_property(model, name, origin_futures)
+            else:
+                query.filter(cls.destin == key)
+                destin_keys = [relation.origin for relation in query.fetch()]
+                destin_futures = ndb.get_multi_async(destin_keys)
+                model = model_future.get_result()
+                cls._set_destin_property(model, name, destin_futures)
             return model
 
         return model_future.get_result()
 
     @classmethod
-    def _set_relation_property(cls, model, name, origin_futures):
-        setattr(model, name, [f.get_result() for f in origin_futures])
+    def _set_destin_property(cls, model, name, futures):
+        setattr(model, name, [f.get_result() for f in futures])
+
+    @classmethod
+    def _set_origin_property(cls, model, name, futures):
+        setattr(model, name, [f.get_result() for f in futures])
 
 
 class OneToMany(Relation):
     @classmethod
-    def _set_relation_property(cls, model, name, origin_futures):
-        setattr(model, name, origin_futures[0].get_result())
+    def _set_destin_property(cls, model, name, futures):
+        setattr(model, name, futures[0].get_result())
