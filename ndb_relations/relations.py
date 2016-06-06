fdbe9fcdb4aa9b9be4882aee03d7aea451b71af6
renzon <renzon@gmail.com>
2016-06-06 00:23:48 -0300

    close #4 running searchs in parallel

--- ndb_relations/relations.py
+++ ndb_relations/relations.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+from functools import partial
+
 from google.appengine.ext import ndb
 from google.appengine.ext.ndb.polymodel import PolyModel
 
@@ -18,6 +20,7 @@ class Relation(PolyModel):
     def _set_origin_property(cls, model, name, futures):
         setattr(model, name, [f.get_result() for f in futures])
 
+    # It is overwriting the method so it exposes the relation class to fetch function
     @classmethod
     def query(cls, *args, **kwds):
         q = super(Relation, cls).query(*args, **kwds)
@@ -44,21 +47,29 @@ class OneToMany(Relation):
 def fetch(key, *args):
     model_future = key.get_async()
     model = None
+    partials = []
+
     for tpl in args:
         name, query = tpl
         cls = query._relation_cls
+
         if key.kind() == cls.origin._kind:
             query.filter(cls.origin == key)
             destin_keys = [relation.destin for relation in query.fetch()]
             destin_futures = ndb.get_multi_async(destin_keys)
             model = model or model_future.get_result()
-            cls._set_origin_property(model, name, destin_futures)
+            p = partial(cls._set_origin_property, model, name, destin_futures)
 
         else:
             query.filter(cls.destin == key)
             origin_keys = [relation.origin for relation in query.fetch()]
             origin_futures = ndb.get_multi_async(origin_keys)
             model = model or model_future.get_result()
-            cls._set_destin_property(model, name, origin_futures)
+            p = partial(cls._set_destin_property, model, name, origin_futures)
+
+        partials.append(p)
+
+    for p in partials:
+        p()
 
     return model or model_future.get_result()
